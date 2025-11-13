ebcb6e0fa560a28564594e288a19cb4024908377
ReedL-corvidae <rclinehan@email.neit.edu>
2025-11-13 09:41:42 -0500

    game loop added

--- code.js
+++ code.js
@@ -1,11 +1,17 @@
 import { Toolbox } from "./toolbox.js";
 
+
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 let toolbox = new Toolbox();
 
 let myFavoriteLetters = ["a", "b", "c"];
 
+//game loop
+function gameLoop(){
+
+}
+setInterval(gameLoop, 50);
 
 // let counts = [0,0,0];
 // for(let i = 0; i < 100; i++) {
